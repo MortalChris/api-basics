f772342d1bdf7e3861c1f9dd7ba9b54995e6e6c6
Christian V. <88056810+MortalChris@users.noreply.github.com>
2024-06-12 12:31:05 +0000

    added post, path to index html

--- rest-service-node/index.js
+++ rest-service-node/index.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -5,12 +6,17 @@ const app = express();
 
 app.use(bodyParser.json());
 
-const sayHi = (req, res) => {
-    res.send("Hi!");
-  };
-  
-app.get("/", sayHi);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.post("/add", (req, res) => {
+  const { a, b } = req.body;
+  res.send({
+    result: parseInt(a) + parseInt(b)
+  });
+});
 
 app.listen(5000, () => {
-  console.log(`Server is running on port http://localhost/5000.`);
+  console.log(`Server is running on port 5000.`);
 });
